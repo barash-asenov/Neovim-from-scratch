e945f1d21356fa103e490ce7ffd67e1bcd6180dd
Barash Asenov <asenov@carlfinance.de>
2022-09-01 13:25:09 +0200

    [add] Personal config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,21 @@
-vim.cmd [[
-try
-  colorscheme darkplus
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+local colorscheme = 'onedark'
+
+local status_ok, theme = pcall(require, colorscheme)
+
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+theme.setup { style = 'darker' }
+theme.load()
+
+-- Old way of setting color schemes
+-- vim.cmd [[
+-- try
+--   colorscheme onedark
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]]
